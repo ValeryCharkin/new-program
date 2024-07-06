6479bd6d1db99c98edac5985cb35bcac99332a43
ValeryCharkin <charkin.valery@gmail.com>
2024-07-07 00:12:19 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+//TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+public class Main {
+    public static void main(String[] args) {
+
+        int amaunt = 20000;//стоимость билета
+        int i1 = 20;// рублей для одной бонусной мили
+        int bonus = amaunt / i1;
+        {
+
+            System.out.println(amaunt/i1);
+
+
+        }
+    }
+}
