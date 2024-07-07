f27c63e9c68f9bb0b1143ee9d8944501b2e8b9e1
ValeryCharkin <charkin.valery@gmail.com>
2024-07-07 13:48:03 +0300

    change

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
-
-//TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-public class Main {
+ public class Main {
     public static void main(String[] args) {
 
         int amaunt = 20000;//стоимость билета
