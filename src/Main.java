755333b8fda65c22c80b9c021eda32e5d9d063e8
ValeryCharkin <charkin.valery@gmail.com>
2024-07-07 14:00:02 +0300

    change3

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
  public class Main {
-=======
-public class Main {
->>>>>>> b42ddc3072cd0e58cfa28ca3fc8e659fbea22dcc
     public static void main(String[] args) {
 
         int amaunt = 20000;//стоимость билета
